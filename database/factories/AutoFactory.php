b8b804bbb3f12fc58989d90b663176ab9fca9efc
Juan Fernández <j.fernandez.munoz@hotmail.com>
2018-12-18 00:31:42 -0300

    Trigger usuario-rol y faker de automovil arreglado

--- database/factories/AutoFactory.php
+++ database/factories/AutoFactory.php
@@ -5,16 +5,16 @@ use Faker\Generator as Faker;
 $factory->define(App\Auto::class, function (Faker $faker) {
     $ids_empresa = \DB::table('empresas')->select('id')->get();
     $id_empresa = $ids_empresa->random()->id;
-    // $faker = (new \Faker\Factory())::create();
-    // $faker->addProvider(new \Faker\Provider\Fakecar($faker));
+    $faker = (new \Faker\Factory())::create();
+    $faker->addProvider(new \Faker\Provider\Fakecar($faker));
     
 
     return [
         'numero_puertas' => $faker->randomDigit(2,4),
         'tipo_transmision' => $faker->numberBetween(0,1),
         'numero_asientos' =>$faker->numberBetween(2,4),
-        'modelo' =>$faker->realtext(50),
-        'marca' =>$faker->realtext(30),
+        'modelo' =>$faker->vehicleModel,
+        'marca' =>$faker->vehicleBrand,
         'disponibilidad' =>$faker->boolean,
         'empresa_id' =>$id_empresa,
     ];
